06d9059c018c97e6cd9456d1ddd7227eea58bc30
zekepari <pariezekiel@gmail.com>
2024-10-11 13:10:56 +1000

    this how we gotta do it

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import dropbox
 from gpiozero import Button, LED
 from flask import Flask, Response, render_template
@@ -10,7 +11,7 @@ import json
 app = Flask(__name__)
 
 # Initialize Dropbox client with access token
-DROPBOX_ACCESS_TOKEN = 'your-dropbox-access-token'
+DROPBOX_ACCESS_TOKEN = os.getenv('DROPBOX_ACCESS_TOKEN')
 dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
 
 # Initialize sensors
@@ -118,7 +119,7 @@ def get_sensor_data():
     except RuntimeError as error:
         print(f"Error reading from sensors: {error}")
         return {}
-
+#.B-g6esU6NcBJG7bdVAYb3QPCYHhHgXrL7GMc8Yht2_p72QgzKeC9PjjIXn1S95v4_FKW_rN7I88QH4qpW-lGE5TSsnltzCDiSTBycCI0Imv76aPyJ5eWPTjjs8eQXSxDKnYHw7WbszW-zEONNht9PQM
 # Function to upload data to Dropbox
 def upload_to_dropbox(data):
     file_name = f"sensor_data_{int(time())}.json"
